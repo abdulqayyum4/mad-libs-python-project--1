5a73e4bd2ec45a28c260762409a0bef3977b488a
Abdul Qayyum <160266329+abdulqayyum4@users.noreply.github.com>
2025-07-02 13:14:57 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+ #Mad Libs Python Project -1
+
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+#Mab Libs Story Paragraph
+story = f"one day, {name} went to {place}. It was a {adjective} place. There {name} found a {noun} and decided to {verb} with it."
+
+print("\nHere is your Mad Libs Story:")
+print(story)
